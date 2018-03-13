bd981525e1f4e51c6097bdb9a4389e307f440eed
Maxwell <maxwell.acioli@gmail.com>
2018-03-13 11:07:22 -0300

    init commit

---
+++ src/main/java/br/com/ic/analyzer/Analyzer2M.java
@@ -0,0 +1,25 @@
+package br.com.ic.analyzer;
+
+import br.com.ic.lexical.LexicalAnalyzer;
+import br.com.ic.syntactic.SyntaticAnalyzer;
+
+
+public class Analyzer2M {
+	private static LexicalAnalyzer lexicalAnalyzer;
+	private static SyntaticAnalyzer syntaticAnalyzer;
+
+	private static String filePath = "src/files/input/hello.2m";
+
+	public Analyzer2M() {
+
+	}
+
+	public static void main(String[] args) {
+
+		lexicalAnalyzer = new LexicalAnalyzer(filePath);
+		lexicalAnalyzer.readFile();
+		
+		syntaticAnalyzer = new SyntaticAnalyzer(lexicalAnalyzer);
+		syntaticAnalyzer.analyze();
+	}
+}
