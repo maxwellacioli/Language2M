e1507c416792c99c834af06e33ded1dedebacc6d
Maxwell <maxwell.acioli@gmail.com>
2018-11-29 17:29:04 -0300

    Adição do código exemplo que calculta o menor entre dois números

--- src/main/java/analyzer/Analyzer2M.java
+++ src/main/java/analyzer/Analyzer2M.java
@@ -12,7 +12,7 @@ import static org.bytedeco.javacpp.LLVM.*;
 public class Analyzer2M {
 	private static SyntaticAnalyzer syntaticAnalyzer;
 
-	private static String filePath = "files/input/calculator.2m";
+	private static String filePath = "files/input/less.2m";
 
 	public Analyzer2M() { }
 
